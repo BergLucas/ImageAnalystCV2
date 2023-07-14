497b336ffc00b4cbb22acf39758ec8cc809bb600
Lucas Berg <55436804+BergLucas@users.noreply.github.com>
2023-07-14 19:58:17 +0200

    Force cv2.dnn module

--- src/image_analyst/cv2/__init__.py
+++ src/image_analyst/cv2/__init__.py
@@ -1,5 +1,6 @@
 try:
     import cv2  # noqa: F401
+    import cv2.dnn  # noqa: F401
 except ImportError as e:
     raise ImportError(
         "OpenCV library is not found. Please install OpenCV first."
